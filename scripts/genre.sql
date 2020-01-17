4dda286d93a3867e001db5c69d10e9fec223b203
giuliaromualdi <58422985+giuliaromualdi@users.noreply.github.com>
2020-01-17 15:50:40 +0100

    Add files via upload

---
+++ scripts/genre.sql
@@ -0,0 +1,19 @@
+SELECT * FROM blue.genres;
+
+drop table if exists genres;
+
+create table genres(
+genre_id integer primary key auto_increment,
+type varchar(20));
+
+insert into genres(genre_type)
+values('English literature');
+
+insert into genres(genre_type)
+values('Spanish literature');
+
+insert into genres(genre_type)
+values('Russian literature');
+
+insert into genres(genre_type)
+values('Italian literature');
